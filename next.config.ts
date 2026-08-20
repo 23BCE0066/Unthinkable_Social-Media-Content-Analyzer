42f601d18b04f22370d5e15b9527020cf61cfe9a
Mehul Goyal <mehul.goyal@example.com>
2026-08-20 22:56:20 +0530

    fix: next 16 config api for vercel

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  experimental: {
-    serverComponentsExternalPackages: ['pdf-parse', 'tesseract.js'],
-  }
+  serverExternalPackages: ['pdf-parse', 'tesseract.js'],
 };
 
 export default nextConfig;
